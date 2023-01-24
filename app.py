aa3f3f226925f636a11864485771eb73f6eb54a1
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2023-01-24 15:41:01 +0100

    commit

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, jsonify, request
+import os
+from neo import Database
 
 app = Flask(__name__)
 
 
+uri = os.getenv('URI')
+user = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+database = Database(uri, user, password)
+
+
 @app.route("/employees", methods=['GET'])
 def getEmployees():
-    return jsonify('test')
+    employees = database.getEmployees()
+    result = [{'name': result['m']['name']} for result in employees]
+    return jsonify(result)
 
 
 @app.route("/employees", methods=['POST'])
 def addEmployee():
     dane = request.get_json()
-    return jsonify('test')
+    name = dane['name']
+    department = dane['department']
+    result = database.addEmployee(name, department)
+    return jsonify(result)
 
 
 @app.route("/employees/:id", methods=['PUT'])
--- neo.py
+++ neo.py
@@ -5,3 +5,29 @@ class Database:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def getEmployees(self):
+        with self.driver.session() as session:
+            return session.read_transaction(self._get_employees)
+
+    @staticmethod
+    def _get_employees(tx):
+        result = tx.run('MATCH (m:Employee) RETURN m').data()
+        return result
+
+    def addEmployee(self, name, department):
+        with self.driver.session() as session:
+            return session.execute_write(self._add_employee, name, department)
+
+    @staticmethod
+    def _add_employee(tx, name, department):
+        result = tx.run("CREATE (a:Employee) "
+                        "SET a.name = $name, "
+                        "SET a.department = $department "
+                        "RETURN a", name=name, department=department)
+        return result.single()
+
+    @staticmethod
+    def _delete_employee(tx, id):
+        result = tx.run('MATCH (m:Employee) WHERE m.id=$id DETACH DELETE m')
+
